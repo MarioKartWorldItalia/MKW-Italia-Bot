6b632f9c17d72ff10042bf5d977bf4ce85a1f59c
Simone Antonacci <simo.antonacci@gmail.com>
2025-07-13 03:02:42 +0000

    Handle dell'interrupt di EXIT

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 import { Application } from "./application";
 import { log } from "./logging/log.js";
 
-process.on("uncaughtException", (e) => {
-    handleError(e);
-});
-
-process.on("unhandledRejection", (e) => {
-    if (e instanceof Error) {
-        handleError(e);
-    } else {
-        handleError(new Error(String(e)));
-    }
-});
-
-main();
-
 function handleError(error: Error) {
     log(`\nFATAL ERROR:\n{error.message}\n`);
     log(`Stack trace:\n${error.stack}`);
@@ -22,6 +8,25 @@ function handleError(error: Error) {
 }
 
 function main() {
+    process.on("uncaughtException", (e) => {
+        handleError(e);
+    });
+
+    process.on("unhandledRejection", (e) => {
+        if (e instanceof Error) {
+            handleError(e);
+        } else {
+            handleError(new Error(String(e)));
+        }
+    });
+
+    process.on("exit", (exitCode) => console.log("Exiting process with code " + exitCode));
+
+
     const app = new Application();
     app.start();
 }
+
+//-------------ENTRY POINT----------------
+main();
+//----------------------------------------

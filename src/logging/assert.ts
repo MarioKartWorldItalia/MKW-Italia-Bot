6ebd90cf1e802ba6d1f57a71be53d7542d0357f2
Simone Antonacci <simo.antonacci@gmail.com>
2025-11-23 23:43:14 +0000

    Refactor assertion error handling to use custom AssertError class

--- src/logging/assert.ts
+++ src/logging/assert.ts
@@ -1,10 +1,17 @@
+    export class AssertError extends Error {
+        constructor(msg: string) {
+            super(msg);
+            this.name = "AssertError";
+        }
+    }
+    
     export function assertCond(cond: boolean, message?: string) {
         if (!cond) {
             let errStr = "Assertion failed";
             if(message && message.length > 0) {
                 errStr += "\nMessage: " + message;
             }
-            throw new Error(errStr);
+            throw new AssertError(errStr);
         }
     }
 

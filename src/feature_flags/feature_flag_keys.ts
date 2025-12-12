378bfd4d88c19105cf6e2c9b491e90fd341f0a04
Simone Antonacci <simo.antonacci@gmail.com>
2025-12-12 18:07:57 +0000

    Add feature flags

--- src/feature_flags/feature_flag_keys.ts
+++ src/feature_flags/feature_flag_keys.ts
@@ -1,3 +1,5 @@
 export enum FeatureFlagKeys {
     ExitOnUnhandledError = "exit_process_on_unhandled_error",
+    UpdateSlashCommands = "update_slash_commands",
+    AutoDisalbeUpdateSlashCommands = "auto_disable_update_slash_commands",
 }

5cf27bcde4758d1877a11aff263294558ec51867
Simone Antonacci <simo.antonacci@gmail.com>
2025-12-20 11:48:59 +0000

    Add more role options in "resetta_ruolo"

--- src/interaction/util_commands.ts
+++ src/interaction/util_commands.ts
@@ -1,4 +1,4 @@
-import { ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, Client, Interaction, Message, MessageFlags, SlashCommandBuilder } from "discord.js";
+import { ActionRow, ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, Client, inlineCode, Interaction, Message, MessageFlags, SlashCommandBuilder } from "discord.js";
 import { log, logError } from "../log";
 import { Application } from "../application";
 import { resetRole } from "../utils";
@@ -11,11 +11,43 @@ export async function bindGeneralCommands(client: Client): Promise<Map<string, (
                 .setName("resetta_ruolo")
                 .setDescription("Rimuove il ruolo da tutti gli utenti")
                 .addRoleOption(roleOption => {
-                    roleOption.setName("ruolo");
+                    roleOption.setName("ruolo_1");
                     roleOption.setDescription("Seleziona il ruolo");
                     roleOption.setRequired(true);
                     return roleOption;
                 })
+                .addRoleOption(roleOption => {
+                    roleOption.setName("ruolo_2");
+                    roleOption.setDescription("Ruolo 2");
+                    roleOption.setRequired(false);
+                    return roleOption;
+                })
+                .addRoleOption(roleOption => {
+                    roleOption.setName("ruolo_3");
+                    roleOption.setDescription("Ruolo 3");
+                    roleOption.setRequired(false);
+                    return roleOption;
+                })
+                .addRoleOption(roleOption => {
+                    roleOption.setName("ruolo_4");
+                    roleOption.setDescription("Ruolo 4");
+                    roleOption.setRequired(false);
+                    return roleOption;
+                })
+                .addRoleOption(roleOption => {
+                    roleOption.setName("ruolo_5");
+                    roleOption.setDescription("Ruolo 5");
+                    roleOption.setRequired(false);
+                    return roleOption;
+                })
+                .addRoleOption(roleOption => {
+                    roleOption.setName("ruolo_6");
+                    roleOption.setDescription("Ruolo 6");
+                    roleOption.setRequired(false);
+                    return roleOption;
+                }
+        )
+        .toJSON()
         )
     );
     Promise.all(commandsPromises).then(() => log("Comandi di util aggiornati")).catch(logError);
@@ -30,7 +62,12 @@ async function onRoleReset(interaction: Interaction) {
         return;
     }
 
-    const role = interaction.options.getRole("ruolo", true);
+    let roles = [];
+    for(let i = 0; i < 6; i++) {
+        const role = interaction.options.getRole(`ruolo_${i + 1}`);
+        if (role) roles.push(role);
+    }
+
     const confirmBtn = new ButtonBuilder()
         .setCustomId("confirm")
         .setLabel("Conferma")
@@ -43,7 +80,7 @@ async function onRoleReset(interaction: Interaction) {
 
     const reply = await interaction.reply(
         {
-            content: `Conferma reset del ruolo ${role.name}, con id ${role.id}`,
+            content: `Conferma reset dei ruoli: ${roles.map(r => inlineCode(r.name)).join(", ")}?`,
             components: [new ActionRowBuilder().addComponents(confirmBtn, abortBtn).toJSON()],
             flags: MessageFlags.Ephemeral
         }
@@ -52,9 +89,11 @@ async function onRoleReset(interaction: Interaction) {
     const response = await reply.awaitMessageComponent();
     if (response.customId == "confirm") {
         await response.deferReply({flags: MessageFlags.Ephemeral});
-        await resetRole(role.id);
+        for(const role of roles) {
+            await resetRole(role.id);
+        }
         await response.editReply({
-            content: "Ruolo resettato: " + role.name,
+            content: "Ruoli resettati: " + roles.map(r => r.name).join(", "),
         })
     }
 
